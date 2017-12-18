efeee2c125217c13cd2008ea7f5e49d4da00ca03
SeaJayGames <34647658+SeaJayGames@users.noreply.github.com>
2017-12-18 10:54:24 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	str name
+	cout << "Hello! What's your name?"; << endl;
+	cin >> name >> endl;
+	cout << "Oh, hi " << name << "." << endl;
+}
